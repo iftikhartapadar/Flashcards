ed21c7f03032802c14ff4b793636902c74e63f16
iftikhartapadar <iftikhartapadar@gmail.com>
2023-03-07 02:21:15 -0500

    finished project

---
+++ Flashcards/components/CardList.jsx
@@ -0,0 +1,63 @@
+const title = "Learn Popular Chess Openings!";
+const subtitle = "What are the following chess openings?";
+
+// Difficulties: easy, medium, hard
+// Image: include url
+const cardList = [
+    {
+      difficulty: "easy",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR&piece=neo&size=2",
+      answer: "Sicillian Defense",
+    },
+    {
+      difficulty: "easy",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR&piece=neo&size=2",
+      answer: "French Defense",
+    },
+    {
+      difficulty: "easy",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR&piece=neo&size=2",
+      answer: "Caro-Kann Defense",
+    },
+    {
+      difficulty: "medium",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R&piece=neo&size=2",
+      answer: "Italian Game",
+    },
+    {
+      difficulty: "medium",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR&piece=neo&size=2",
+      answer: "Scandinavian Defense",
+    },
+    {
+      difficulty: "medium",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=rnbqkb1r/ppp1pppp/3p1n2/8/3PP3/8/PPP2PPP/RNBQKBNR&piece=neo&size=2",
+      answer: "Pirc Defense",
+    },
+    {
+      difficulty: "hard",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=rnbqkbnr/pppp1ppp/8/4p3/4P3/2N5/PPPP1PPP/R1BQKBNR&piece=neo&size=2",
+      answer: "Vienna Game",
+    },
+    {
+      difficulty: "hard",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=rnbqkbnr/pppp1ppp/8/4p3/4PP2/8/PPPP2PP/RNBQKBNR&piece=neo&size=2",
+      answer: "King's Gambit",
+    },
+    {
+      difficulty: "hard",
+      question: "What chess opening is this?",
+      image: "https://www.chess.com/dynboard?board=green&fen=rnbqkbnr/ppp1pppp/8/3p4/2PP4/8/PP2PPPP/RNBQKBNR&piece=neo&size=2",
+      answer: "Queen's Gambit",
+    },
+  ];
+
+export { title, subtitle, cardList };
--- Flashcards/src/App.jsx
+++ Flashcards/src/App.jsx
@@ -1,32 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import {useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Card from "../components/Card"
+import { title, subtitle, cardList } from "../components/CardList"
 
+
+/* Random number from 0 to 1 less than max */
+function random(max) { 
+  return Math.floor(Math.random() * (max))
+}
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  /* Go through cards random */
+  const updateCount = () => {
+    setCount(random(cardList.length))
+  }
+  
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <h1>{title}</h1>
+        <h3>{subtitle}</h3>
+        <h4>Number of Flashcards: {cardList.length}</h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <Card props={cardList[count]}/>
+      <div className="button-container">
+        <button className="button" onClick={updateCount}>⮕</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
